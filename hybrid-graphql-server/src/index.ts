00f7045bdfb4a09a34815e8f1abc17ab68233796
aihki <laine.nestori.julius@gmail.com>
2024-03-18 12:42:58 +0200

    Enable HTTPS in production environment

--- hybrid-graphql-server/src/index.ts
+++ hybrid-graphql-server/src/index.ts
@@ -3,17 +3,17 @@ import https from 'https';
 import fs from 'fs';
 
 
-// if (process.env.NODE_ENV === 'production') {
-//  const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
-//  const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+if (process.env.NODE_ENV === 'production') {
+ const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+ const sslcert = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
 
-//  const options = {
-//    key: sslkey,
-//    cert: sslcert,
-//  };
-//  const httpsPort = process.env.HTTPS_PORT || 8000;
-//  https.createServer(options, app).listen(httpsPort);
-// }
+ const options = {
+   key: sslkey,
+   cert: sslcert,
+ };
+ const httpsPort = process.env.HTTPS_PORT || 8000;
+ https.createServer(options, app).listen(httpsPort);
+}
 
 const port = process.env.PORT || 3000;
 
